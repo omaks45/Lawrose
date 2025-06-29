aa5fe7bc8835e32e9635249b06b979c46e3f6e69
omaks45 <danielgozie5@gmail..com>
2025-06-29 14:56:22 +0100

    service module updated

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,9 +1,28 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
-import { UsersService } from './users.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from './user.schema';
+import { Address, AddressSchema } from './address.schema';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { UsersRepository } from './repository/user.repository';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: User.name, schema: UserSchema },
+      { name: Address.name, schema: AddressSchema }
+    ])
+  ],
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [
+    UsersService,
+    UsersRepository
+  ],
+  exports: [
+    UsersService,
+    UsersRepository,
+    MongooseModule
+  ]
 })
 export class UsersModule {}
